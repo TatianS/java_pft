61253c847b5e1b29476595c588583b9e1a04fd74
TatianS <9119564@gmail.com>
2018-05-22 10:40:17 +0300

    Update project files

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/helloworld.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class helloworld {
+  public static void main(String[] args) {
+    hello("world");
+    hello("Tanja");
+  }
+    public static void hello(String somebody) {
+      System.out.println("Hello, " + somebody + "!");
+    }
+  }
